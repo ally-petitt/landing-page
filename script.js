5059889041384ffd6bbb32c08001d122f7051bc9
ally-petitt <allypetitt@gmail.com>
2021-03-17 14:08:02 -0700

    svgs now respond on click

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 /* card transition */
 $(".card").click((e) => {
-  const card = e.target.closest(".card").classList[1];
+  const cardClass = e.target.closest(".card").classList[1];
 
   let cardInfo = {
-    cardBubble: $("." + card + " .card-bubble"),
+    cardBubble: $("." + cardClass + " .card-bubble"),
+    svgContainerClass: $("." + cardClass + " .svg-container")
+      .attr("class")
+      .split(" ")[1],
     largeCircle: "circle(300px at 90% -10%)",
     smallCircle: "circle(150px at 90% 10%)",
   };
@@ -15,10 +18,18 @@ $(".card").click((e) => {
   }
 });
 
-function setCard({ cardBubble, largeCircle }) {
+function setCard({ cardBubble, largeCircle, svgContainerClass }) {
   cardBubble.css("clip-path", largeCircle);
+  $("." + svgContainerClass + " > svg").attr({
+    width: "100",
+    height: "100",
+  });
 }
 
-function resetAllCards({ cardBubble, smallCircle }) {
+function resetAllCards({ cardBubble, smallCircle, svgContainerClass }) {
   cardBubble.css("clip-path", smallCircle);
+  $("." + svgContainerClass + " svg").attr({
+    width: "150",
+    height: "150",
+  });
 }
